46294a96348311e1d870d0c2314f1eda7f970771
Maxime Delpit <maximedelpit@gmail.com>
2017-11-01 17:14:18 +0100

    set google tag mger id on links & btns

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def gtm_suffix_link
+    if params[:controller] == 'devise/sessions' && params[:action] == 'new'
+      return '-login'
+    elsif params[:controller] == 'devise/registrations' && params[:action] == 'new'
+      return '-signup'
+    else
+      return '-other'
+    end
+  end
 end
