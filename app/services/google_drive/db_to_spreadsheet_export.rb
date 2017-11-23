4e548d26acec0ff942c44c6dbfa05a2a6b2cfc54
Maxime Delpit <maximedelpit@gmail.com>
2017-11-23 11:50:46 +0100

    gdrive update_all

--- app/services/google_drive/db_to_spreadsheet_export.rb
+++ app/services/google_drive/db_to_spreadsheet_export.rb
@@ -6,7 +6,32 @@ module GoogleDrive
     end
 
     def perform(wsheet_title)
+      @ws = @spreadsheet.worksheet_by_title(wsheet_title)
+      @klass = wsheet_title.classify.constantize
+      update_data
+    end
 
+    private
+
+    def update_data
+      # we skip first sheet row so we substract 1 to our resource array
+      generate_header
+      row = 2
+      @klass.find_each do | resource |
+        values = resource.attributes.values
+        (1..@ws.num_cols).each do |col|
+          @ws[row, col] = values[col-1]
+        end
+        row += 1
+      end
+      @ws.save
+    end
+
+    def generate_header
+      @klass.column_names.each_with_index do |col_name, i|
+        i += 1
+        @ws[1, i] = col_name
+      end
     end
   end
 end
