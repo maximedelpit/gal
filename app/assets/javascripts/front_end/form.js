8fdf3ffee499af15f7acb09e2aa8e7ca4ab4a205
Maxime Delpit <maximedelpit@gmail.com>
2017-11-27 18:28:36 +0100

    disable form autosubmit

---
+++ app/assets/javascripts/front_end/form.js
@@ -0,0 +1,8 @@
+// Disable JQUERY form autosubmit
+$('form').on('keyup keypress', function(e) {
+  var keyCode = e.keyCode || e.which;
+  if (keyCode === 13) {
+    e.preventDefault();
+    return false;
+  }
+});
