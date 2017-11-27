ecefcf6e0931e6b51bcb184ce5627ed07d6dc226
Maxime Delpit <maximedelpit@gmail.com>
2017-11-27 18:03:23 +0100

    optional indsutry

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:linkedin]
-  belongs_to :industry
+  belongs_to :industry, optional: true
   has_many :subcategories, dependent: :destroy
   has_many :industry_subcategories, through: :subcategories
   has_many :zones, dependent: :destroy
