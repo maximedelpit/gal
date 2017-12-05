08342c3ffc98f3c204b0285cf1e947c1856a7696
Maxime Delpit <maximedelpit@gmail.com>
2017-12-05 14:46:00 +0100

    upsert drive

--- app/services/google_drive/upsert_in_spreadsheet.rb
+++ app/services/google_drive/upsert_in_spreadsheet.rb
@@ -11,12 +11,23 @@ module GoogleDrive
 
     def upsert_data
       @list = @ws.list
-      @row = @ws.num_rows + 1
+      # @row = @ws.num_rows + 1
       @query.find_each do | resource |
-        row = @list.select {|el| el['id'] == resource.id}.first
+        row = @list.select {|el| "#{el['id']}" == "#{resource.id}"}.first
         row ? row.replace(build_row(resource)) : @list.push(build_row(resource))
       end
       @ws.save
     end
   end
 end
+
+
+# Lead.find_each do |l|
+#   l.user_id = 6 if l.user_id.nil?
+#   l.state = 'created'
+#   l.build_status = 'active'
+#   l.save
+# end
+# Lead.find_each do |l|
+#   l.update(created_at: l.created_at - 10.seconds)
+# end
