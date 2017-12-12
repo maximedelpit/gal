822a364694d91bcb3bc74412974f1168c588262e
Maxime Delpit <maximedelpit@gmail.com>
2017-12-12 17:32:43 +0100

    refacto scss js

--- app/assets/javascripts/front_end/custom_select2.js
+++ app/assets/javascripts/front_end/custom_select2.js
@@ -7,20 +7,4 @@ $('.select2').select2({
   tags: true,
   minimumInputLength: 1,
   tokenSeparators: [","]//,
-  // createSearchChoice: function(term) {
-  //   debugger;
-  //   return {
-  //     id: null,
-  //     name: term + ' (new)'
-  //   };
-  // }
-})
-// .on("change", function(e) {
-//   var isNew = $(this).find('[data-select2-tag="true"]');
-//   if(isNew.length){
-//     isNew.replaceWith('<option selected value="'+isNew.val()+'">'+isNew.val()+'</option>');
-//     // $.ajax({
-//     //     // ... store tag ...
-//     // });
-//   }
-// });
+});
