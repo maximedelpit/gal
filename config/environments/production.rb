f291c8a66fb6f41f2e57fb708f5eb69936e428a8
Maxime Delpit <maximedelpit@gmail.com>
2018-01-18 10:09:09 +0100

    trad

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://getalead.io/" }
+  config.action_mailer.default_url_options = { host: "https://www.app.getalead.io/" }
   config.action_mailer.delivery_method = :mailjet
 
   # Settings specified here will take precedence over those in config/application.rb.
