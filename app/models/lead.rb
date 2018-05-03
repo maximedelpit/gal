a9cf65eaffe102a9b2fcb381cbf8057933851b93
Maxime Delpit <maximedelpit@gmail.com>
2018-05-03 13:05:33 +0100

    popover bases

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -16,7 +16,7 @@ class Lead < ApplicationRecord
   # Description step validation
   validates :description, presence: true, if: -> {validation_for?(:description) || active?}
   validates :description, length: {
-    in: 10..10000,
+    in: 10..2000,
     too_short: "%{count} characters is the minimum allowed",
     too_long: "%{count} characters is the maximum allowed"
   }, if: -> {validation_for?(:description) || active?}
